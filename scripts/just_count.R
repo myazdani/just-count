343bb36b60a8fa99c107eabd04c607ac301693a8
Mehrdad Yazdani <myazdani@gmail.com>
2015-07-20 10:55:27 -0700

    find first names from emails and save results

--- scripts/just_count.R
+++ scripts/just_count.R
@@ -4,14 +4,19 @@ library(dplyr)
 library(plotly)
 setwd("~/Documents/misc_work/just_count/")
 
+
+## data processed and provided by Arne Hendrik Schulz
+## http://www.ahschulz.de/enron-email-data/
 load("./data/enron_mysqldump.RData")
 
+## basic word count function
 word.count <- function(x, word){
   res = unlist(strsplit(x, split = " "))
   num.word = length(which(res %in% word))
   return(num.word)
 }
 
+## key stats
 message$just.count = sapply(message$body, FUN = function(txt) word.count(tolower(txt), "just") )
 message$word.count = sapply(message$body, FUN = function(txt) length(unlist(strsplit(txt, split = " "))))
 message$char.count = sapply(message$body, nchar)
@@ -27,6 +32,8 @@ message$beer.count = sapply(message$body, FUN = function(txt)  word.count(tolowe
 message$enron.count = sapply(message$body, FUN = function(txt)  word.count(tolower(txt), "enron") )
 message$chat.count = sapply(message$body, FUN = function(txt)  word.count(tolower(txt), "chat") )
 
+
+## aggregate counts by sender
 message %>%
   group_by(sender) %>%
   summarise(num.emails = n(),median.just.count = median(as.numeric(just.count)),
@@ -42,17 +49,14 @@ message %>%
 res$email.type = "other"
 res[grep("enron", res$sender),"email.type"] = "enron"
 
+## select only senders that are "active" in data set
 res.frequent = subset(res, num.emails > 4) # 5 or more emails gives us roughly 25% of all email addresses sent in data
 
 py <- plotly()
 p = ggplot(res.frequent, aes(x = median.char.count, y = median.just.count, label = sender)) + geom_text()
-response <- py$ggplotly(p, kwargs=list(filename="just-vs-char-counts", fileopt="overwrite"))
-
+#response <- py$ggplotly(p, kwargs=list(filename="just-vs-char-counts", fileopt="overwrite"))
 
 
 enron.emails = subset(res, email.type == "enron")
-
 p = ggplot(enron.emails, aes(x = median.char.count, y = median.just.count, label = sender)) + geom_text()
-response <- py$ggplotly(p, kwargs=list(filename="just-vs-num-chars", fileopt="overwrite"))
-
 res.merged = merge(res, employeelist, by.x = "sender", by.y = "Email_id")
