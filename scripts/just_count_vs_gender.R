9f4380822fa5e33fc93c88e2d0b53a25f9274ea9
Mehrdad Yazdani <myazdani@gmail.com>
2015-07-21 19:38:11 -0700

    added the rate comparison stats

--- scripts/just_count_vs_gender.R
+++ scripts/just_count_vs_gender.R
@@ -14,7 +14,9 @@ just.genders = merge(just.counts, genders)
 
 just.genders.known = subset(just.genders, gender != "None")
 
-ggplot(subset(just.genders.known, num.emails < 1000001), aes(x = num.emails, y = median.just.count/median.char.count, colour = gender)) + geom_point() -> p
+ggplot(subset(just.genders.known, num.emails < 1000001), aes(x = num.emails, y = median.just.count/median.char.count, colour = gender, label = first.name)) + geom_text(position = "jitter") -> p
+py <- plotly()
+response <- py$ggplotly(p, kwargs=list(filename="just-rates-vs-emails", fileopt="overwrite"))
 
 # over 80 percent of senders sent less than 50 emails
 small.num.emails.females = subset(just.genders.known, num.emails < 50 & gender == "female") 
@@ -24,3 +26,10 @@ female.just.rates = small.num.emails.females$median.just.count/small.num.emails.
 male.just.rates = small.num.emails.males$median.just.count/small.num.emails.males$median.word.count
 
 t.test(female.just.rates, male.just.rates)
+
+
+ggplot(subset(just.genders.known, num.emails < 1000001), aes(x = log10(num.emails), y = median.the.count/median.word.count, colour = gender)) + geom_point() -> p
+
+female.the.rates = small.num.emails.females$median.the.count/small.num.emails.females$median.word.count
+male.the.rates = small.num.emails.males$median.the.count/small.num.emails.males$median.word.count
+t.test(female.the.rates, male.the.rates)
